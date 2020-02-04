f47b703f307b86931a4f73b952fd8fe97614b777
Kayla Wood <kaylaewood@gmail.com>
2020-02-04 12:15:31 -0700

    Install jest

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.locals.title = 'Test Server';
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  response.send('We\'re going to test all the routes!');
+});
