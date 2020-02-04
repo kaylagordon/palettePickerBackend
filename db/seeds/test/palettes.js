238b93f35e01d702381461293ca17aeb9880b486
Kayla Wood <kaylaewood@gmail.com>
2020-02-04 12:44:57 -0700

    Seed testable data

--- db/seeds/test/palettes.js
+++ db/seeds/test/palettes.js
@@ -1,13 +1,28 @@
+const palettesTestData = [
+  {
+    color1: '11111',
+    color2: '222222',
+    color3: '333333',
+    color4: '444444',
+    color5: '555555',
+  },
+  {
+    color1: '1111a',
+    color2: '22222a',
+    color3: '33333a',
+    color4: '44444a',
+    color5: '55555a',
+  },
+  {
+    color1: '1111b',
+    color2: '22222b',
+    color3: '33333b',
+    color4: '44444b',
+    color5: '55555b',
+  }
+];
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = async knex => {
+  await knex('palettes').del()
+  await knex('palettes').insert(palettesTestData);
 };
